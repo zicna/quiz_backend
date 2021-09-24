b0402d12f6b0d7434940ebab70682dc557bebf2a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-24 15:51:05 -0500

    add questions and options methods in CategorySerializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,4 +1,18 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :questions, :options
   has_many :quizzes
+  
+  def questions
+    object.quizzes.map do |quiz|
+      quiz.questions
+    end
+  end
+
+  def options
+    object.quizzes.map do |quiz|
+      quiz.questions.map do |question|
+        question.options
+      end
+    end
+  end
 end
