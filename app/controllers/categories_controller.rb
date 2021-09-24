4a348265e4e25a54916d3d2e235e944b7d78ea89
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-24 14:05:02 -0500

    add categories#index and categories#show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,17 @@
 class CategoriesController < ApplicationController
+    def index
+        categories = Category.all
+
+        render json: categories
+    end
+
+    def show
+        category = Category.find_by(id: params[:id])
+
+        if category
+            render json: category
+        else 
+            render json: {message:{error: "Category not found"}}
+        end 
+    end
 end
