21d5e6bb8d6e6ac268b0268aa0de7d585f3ce97a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-27 17:37:59 -0500

    structure API using 'as_json()' method with options through controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,41 @@
 class CategoriesController < ApplicationController
     def index
         categories = Category.all
+        options = {
+            include: {
+            quizzes:{
+                include:{
+                    questions:{only: [:id, :content, :quiz_id],
+                        include:{
+                            options:{only: [:id, :content, :is_correct,:question_id]}
+                        }
+                    }
+                }, only: [:id, :name, :category_id]
+            }
+        }, only: [:id, :name]
+        }
+        render json: categories.as_json(options)
 
-        render json: categories
     end
 
     def show
         category = Category.find_by(id: params[:id])
+        options = {
+            include: {
+            quizzes:{
+                include:{
+                    questions:{only: [:id, :content, :quiz_id],
+                        include:{
+                            options:{only: [:id, :content, :is_correct,:question_id]}
+                        }
+                    }
+                }, only: [:id, :name, :category_id]
+            }
+        }, only: [:id, :name]
+        }
 
         if category
-            render json: category
+            render json: category.as_json(options)
         else 
             render json: {message:{error: "Category not found"}}
         end 
