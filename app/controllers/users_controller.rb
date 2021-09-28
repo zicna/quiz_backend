abfc9f8f53f0d793d82bd5daa98df78d7a199cd5
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-28 10:06:17 -0500

    add users#show and users#index actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+
+        render json: users.to_json(except: [:created_at, :updated_at])
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user.to_json(except: [:created_at, :updated_at])
+    end
 end
