ba833b0cafd8747fa3a3802da04312e20b09a2a8
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-28 13:41:43 -0500

    quizzes#show, quizzes#index to render json

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,7 +1,29 @@
 class QuizzesController < ApplicationController
+    before_action :options, only: [:index, :show]
     def index
         quizzes =Quiz.all
 
-        render json: quizzes
+        render json: quizzes.as_json(options)
+    end
+
+    def show
+        quiz = Quiz.find_by(id: params[:id])
+        if quiz
+            render json: quiz.as_json(options)
+        else
+            render json: {notice: {error: "Quiz not found"}}
+        end
+    end
+
+    private 
+
+    def options
+        { include: {questions: {
+                        include:{
+                            options:{only: [:id, :question_id, :is_correct, :content]}
+                        }, only: [:id, :content, :quiz_id] 
+        }},
+        only: [:id, :name]
+        }
     end
 end
