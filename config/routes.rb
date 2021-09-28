44e1a3bae4ec7903a0200ae4273ec11bcc89ed11
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-28 13:13:19 -0500

    remove 'categories' from rotes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   resources :options
   resources :questions
   resources :quizzes
-  resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
