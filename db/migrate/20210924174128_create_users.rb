81bc8eef7dd3dc858820903a648fe7d791662069
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-28 09:47:23 -0500

    admin column in users table defaults to false

--- db/migrate/20210924174128_create_users.rb
+++ db/migrate/20210924174128_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.boolean :admin
+      t.boolean :admin, :default => false
 
       t.timestamps
     end
