62aa4b1176a8684c0c6de7f51b39024f4cf4224e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-30 17:00:36 -0500

    add users#create action and private method ''user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,31 @@ class UsersController < ApplicationController
     end
 
     def show
-        user = User.find_by(id: params[:id])
+        user = User.find_by(email: params[:id])
+        if user
+            byebug
         render json: user.to_json(except: [:created_at, :updated_at])
+        end
+    end
+
+    def create
+
+        user = User.find_or_create_by(email: user_params[:email]) do |user|
+            user.email = user_params[:email]
+            user.username = user_params[:username]
+        end
+
+        if user
+            render json: user
+        else
+            render json: {notice: {error: "User not found or created"}}
+        end
+
+
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:username, :userEmail)
     end
 end
