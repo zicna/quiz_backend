7949c7cb74f36fb9c84e3827a6835545606628f3
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-01 13:59:26 -0500

    structure api for user(with users takes, responses and option)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,13 @@ class UsersController < ApplicationController
     def index
         users = User.all
 
-        render json: users.to_json(except: [:created_at, :updated_at])
+        render json: users.as_json(options)
     end
 
     def show
         user = User.find_by(email: params[:id])
         if user
-            byebug
-        render json: user.to_json(except: [:created_at, :updated_at])
+            render json: user.as_json(options)
         end
     end
 
@@ -35,10 +34,19 @@ class UsersController < ApplicationController
     end
 
     def options
-        {
-    include: {responses: {
-        only: [:id, :user_id, :options_id, :question_id, :content]
-    }} 
+        {include: {
+            takes: {
+                include:{
+                    responses:{
+                    include: {option:{
+                        only: [:is_correct]
+                    }},
+                        only: [:id, :content]
+                    }
+                },
+                except: [:created_at, :updated_at]
+            }}, 
+        except: [:created_at, :updated_at]
         }
     end
 end
