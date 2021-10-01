d6ee3859b1e0ab98390518fed21cae6cca298bd6
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-01 13:38:13 -0500

    take down unneeded routes and actions for quizzes controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :options
   resources :takes
   resources :users
-  resources :questions
+  resources :questions, only: :show
   resources :quizzes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
