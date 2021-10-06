9a20a43656ba07e7e7e8b0aa70f82cdbf4f58d10
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-06 10:40:11 -0500

    add create action to respond to fetch request from frontend

--- app/controllers/takes_controller.rb
+++ app/controllers/takes_controller.rb
@@ -1,2 +1,17 @@
 class TakesController < ApplicationController
+    def create
+
+        take = Take.new(take_params)
+
+        if take.save
+            render json: take
+        else
+            render json: {notice: {error: "New Take has not been created."}}
+        end
+    end
+
+    private
+    def take_params
+        params.require(:take).permit(:user_id, :quiz_id)
+    end
 end
