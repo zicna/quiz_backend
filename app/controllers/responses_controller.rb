71ee42c4bdd23f281b3c0a787d951875cf65e36c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-07 08:55:18 -0500

    delete unused code and add comments explaining options one and two

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,37 +1,30 @@
 class ResponsesController < ApplicationController
     def create
-        
         response_params.each do |r|
             resp = Response.create(r)
         end
         #!option one
+        # *this way we only get user and not users takes and responses
         # take_id =params[:_json].first[:take_id]
         # take = Take.find_by(id: take_id)
         # user = User.find_by(id: take.user.id)
         # render json: user
 
-        # !option two redirect to user
+        # !option two redirect to user show action
+        # *all responses that we are sending through params belongs_to same take, therefor they have same user
         take_id =params[:_json].first[:take_id]
         take = Take.find_by(id: take_id)
         user_id = take.user.id
         redirect_to "http://localhost:3000/users/#{user_id}"
         
-        # responses = []
-        # response_params.each do |r|
-        # byebug
-            
-        #     resp = Response.create(r)
-        #     responses << resp
-        # end
-        # render json: responses
     end
 
     private
+
     def response_params
         params.require(:_json).map do |obj|
             obj.permit(:option_id, :question_id, :take_id)
         end
-
     end
     
 end
