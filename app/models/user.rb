960520a32393b7a2e53f1c768b57d45f98c09283
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-07 08:50:19 -0500

    add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     # has_many :responses
     has_many :takes
+    # user model validations
+    validates :username, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
 end
