179919518a07ed52deb646d098737afadbd66596
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-01-23 00:15:52 -0600

    add api/v1/users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::UsersController < ApplicationController
+
+    def create
+        @user = User.create(user_params)
+
+        if @user.valid? 
+            render json: { user: UserSerializer.new(@user) }, status: :created
+        else
+            render json: { error: 'failed to create user' }, status: :unprocessable_entity
+        end
+        
+        private
+
+        def user_params
+            params.require(:user).permit(:username, :password, :email, :admin)
+        end
+end
