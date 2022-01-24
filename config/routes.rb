de2afdaf19134236d3f628f27ce2786886e59ee6
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-01-23 23:22:32 -0600

    add routes for new controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
   resources :responses
   resources :options
   resources :takes
-  resources :users
+  # resources :users
   resources :questions, only: :show
   resources :quizzes
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
